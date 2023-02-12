a48329877fb1c17a86fd4530d7c2b31259e2a867
Menshs <menshs@mail.ru>
2023-02-13 01:15:49 +0300

    Mind_box
    Mind_box

---
+++ Areas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreasPr
+{
+    public class Areas
+    {
+        private static double Sqr(double a)
+        {
+            return a * a;
+        }
+
+        // Площадь круга.
+        public static double CircleArea(double r)
+        {
+            return 3.14f * r * r;
+        }
+
+        // Прямоугольный ли треугольник.
+        public static bool RightTriangle(double a, double b, double c)
+        {
+            a = Math.Abs(a); b = Math.Abs(b); c = Math.Abs(c);
+
+            bool result = false;
+            double[] lines = new double[3] { a, b, c };
+
+            // Ищем сторону треугольника с максимальной длиной.
+            int maxI = 0;
+            for (int i = 1; i < 3; i++)
+                if (lines[i] > lines[maxI])
+                    maxI = i;
+
+            // Если сумма квадратов меньших сторон равна квадрату наибольшей стороны, то треугольник прямоугольный.
+            if (Sqr(lines[maxI]) == Sqr(lines[(maxI + 1) % 3]) + Sqr(lines[(maxI + 2) % 3]))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        // Площадь треугольника.
+        public static double TriangleArea(double a, double b, double c)
+        {
+            a = Math.Abs(a); b = Math.Abs(b); c = Math.Abs(c);
+
+            // Используем формулу Герона.
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        // Невозможно вычислить площадь фигуры без знания типа фигуры.
+    }
+}
---
+++ AreasTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using AreasPr;
+
+namespace AreaTestPr
+{
+    [TestClass]
+    public class AreasTest
+    {
+        [TestMethod]
+        // Тестируем площадь круга.
+        public void TestCircleArea()
+        {
+            double[] r = new double[5] { 5d, 6d, 3d, 4d, 4.5d };
+            double[] result = new double[5] { 78.5d, 113.04d, 28.26d, 50.24d, 63.585d };
+
+            for (int i = 0; i < r.Length; i++)
+            {
+                double act = Areas.CircleArea(r[i]);
+                Assert.AreEqual(result[i], act, 0.01d);
+            }
+        }
+
+        // Тестируем площадь треугольника.
+        public void TestTriangleArea()
+        {
+            double[] a = new double[2] { 3d, 3d };
+            double[] b = new double[2] { 4d, 4.5d };
+            double[] c = new double[2] { 5d, 6d };
+            double[] result = new double[2] { 6d, 6.54d };
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                double act = Areas.TriangleArea(a[i], b[i], c[i]);
+                Assert.AreEqual(result[i], act, 0.01d);
+            }
+        }
+
+        // Тестируем на наличие прямого угла в треугольнике.
+        public void TestRightTriangle()
+        {
+            double[] a = new double[2] { 3d, 3d };
+            double[] b = new double[2] { 4d, 4.5d };
+            double[] c = new double[2] { 5d, 6d };
+            bool[] result = new bool[2] { true, false };
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                bool act = Areas.RightTriangle(a[i], b[i], c[i]);
+                Assert.AreEqual(result[i], act);
+            }
+        }
+    }
+}
